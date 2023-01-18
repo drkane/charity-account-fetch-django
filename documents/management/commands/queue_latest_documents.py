21ff2ab349a49d707c23466c99d8f5eca866612e
David Kane <david@dkane.net>
2023-01-18 13:26:26 +0000

    filters on latest documents

--- documents/management/commands/queue_latest_documents.py
+++ documents/management/commands/queue_latest_documents.py
@@ -3,7 +3,7 @@ from django.db.models import F
 from django_q.tasks import async_task
 
 from documents.fetch import fetch_documents_for_charity
-from documents.models import CharityFinancialYear, FetchGroup
+from documents.models import CharityFinancialYear, FetchGroup, Regulators
 
 
 class Command(BaseCommand):
@@ -26,6 +26,8 @@ class Command(BaseCommand):
         documents = CharityFinancialYear.objects.filter(
             documents__isnull=True,
             income__gt=25000,
+            charity__source=Regulators.CCEW,
+            document_submitted__isnull=False,
         ).order_by(F("income").desc(nulls_last=True))[:n]
         self.stdout.write(str(documents.query))
         for record in documents:
